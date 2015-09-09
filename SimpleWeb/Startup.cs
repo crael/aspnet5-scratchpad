5a6a2f173418be1f70df07d742c8a8532882e84a
Christopher Rael <Christopher.Rael@gmail.com>
2015-09-08 20:28:24 -0700

    First real commit.

---
+++ SimpleWeb/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Http;
+
+namespace SimpleWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello, world!");
+            });
+        }
+    }
+}
