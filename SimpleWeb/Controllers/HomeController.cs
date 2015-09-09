e09b2b145e9032d801026206c11ed3d4cc83a77f
Christopher Rael <Christopher.Rael@gmail.com>
2015-09-09 01:17:28 -0700

    Added configuration and secret stuff.

--- SimpleWeb/Controllers/HomeController.cs
+++ SimpleWeb/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Configuration;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +11,18 @@ namespace SimpleWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+            this.ViewBag.PublicName = this._configuration["PublicName"];
+            this.ViewBag.SecretName = this._configuration["SecretName"];
             return View();
         }
     }
--- SimpleWeb/Startup.cs
+++ SimpleWeb/Startup.cs
@@ -5,14 +5,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.Configuration;
 
 namespace SimpleWeb
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
+
+            // This reads the configuration keys from the secret store.
+            // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+            builder.AddUserSecrets();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            this.Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IConfiguration>(x => this.Configuration);
         }
 
         public void Configure(IApplicationBuilder app)
